05c22c0d2aa8f9a08e1da0f734e730b9eec732f7
jeremy-hubsy <jeremy@hubsy.fr>
2019-08-17 17:36:09 +0200

    setup

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find your location. Try another search.', undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ` It is currently ` + body.currently.temperature + ' degrees out. There is a ' + (body.currently.precipProbability) * 100+ ' % of rain')
+            callback(undefined, body.daily.data[0].summary + ` It is currently ` + body.currently.temperature + ' degrees out. There is a ' + (body.currently.precipProbability) * 100+ ' % chance that you will end up in the rain')
         }
     })
 }
